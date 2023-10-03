2bbbef0c8e34755ec25ece03213f269b32e211b9
Saranassir77 <saraelhakeem2003@gmail.com>
2023-10-02 18:17:02 -0700

    0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,19 +8,19 @@
  */
 int main(int argc, char **argv)
 {
-        int j, mul = 1;
+	int j, mul = 1;
 
-        if (argc > 3 || argc < 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        else
-        {
-                for (j = 1; j <= 2; j++)
-                        mul = mul * atoi(argv[j]);
-                printf("%d\n", mul);
-                return (0);
-        }
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (j = 1; j <= 2; j++)
+			mul = mul * atoi(argv[j]);
+		printf("%d\n", mul);
+		return (0);
+	}
 }
 
